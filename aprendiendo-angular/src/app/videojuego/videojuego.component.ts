5480353c915c52ef7208007fa29ead1efed36d37
Oscar Nelson <oscar.lanza@agilesolutions.com>
2020-01-03 16:04:10 -0600

    Video 170
    Completado hasta carpeta 47, video 170

---
+++ aprendiendo-angular/src/app/videojuego/videojuego.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
+
+@Component({
+    selector: 'videojuego',
+    templateUrl: './videojuego.component.html'
+})
+
+export class VideojuegoComponent implements OnInit, DoCheck, OnDestroy {
+
+    public titulo: string;
+    public listado: string;
+
+    constructor() {
+        this.titulo = "Componente de videojuegos";
+        this.listado = "Listado de los juegos mas populares";
+        console.log("Se ha cargado el componente, bueno: videojuego.component.ts");
+    }
+
+    ngOnInit(){
+        console.log('OnInit ejecutado!!');
+    }
+
+    ngDoCheck(){
+        console.log('DoCheck Ejecutado!!');
+    }
+
+    ngOnDestroy(){
+        console.log('OnDestroy ejecutado!!');
+    }
+
+    cambiarTitulo(){
+        this.titulo = "Nuevo titulo del cpomponente"
+    }
+}
